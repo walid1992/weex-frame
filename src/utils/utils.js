f688d3cdad23504d787499577192ecec514c172f
walid1992 <541280314@qq.com>
2017-03-04 01:52:11 +0800

    删除无用文件

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -4,26 +4,6 @@
  * @description 封装utils工具类使用
  */
 
-let modal = weex.requireModule('modal')
-let navigator = weex.requireModule('navigator')
-
-let data = {
-  debug: true
-}
-
-function toastDebug (self, content) {
-  if (data.debug) {
-    toast(self, content)
-  }
-}
-
-function toast (content) {
-  modal.toast({
-    message: content,
-    duration: 2.0
-  })
-}
-
 function getParameterByName (name, url) {
   name = name.replace(/[\[\]]/g, '\\$&')
   let regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
@@ -38,5 +18,5 @@ function getParameterByName (name, url) {
 }
 
 export default {
-  toastDebug, toast, getParameterByName
+  getParameterByName
 }
