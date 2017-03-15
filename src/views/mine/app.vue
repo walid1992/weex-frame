11191eb83855db26e806ae66c0fef890e2148b85
syswin <ustbzhangli@163.com>
2017-03-15 09:34:23 +0800

    提交我的主页面

--- src/views/mine/app.vue
+++ src/views/mine/app.vue
@@ -7,6 +7,33 @@
   import route from 'constants/route'
 
   export default {
+    data(){
+      return {
+        personName: '张小凡',
+        workPosition: 'web前端工程师',
+        workDepartment: '思源总部/互联集团/应用引擎部',
+        personInfoList: [{
+          iconUrl: 'https://gitlab.com/toonteam/weex/raw/f9492284d15c3d9ec79b72608768d20cf450cae9/src/views/mine/imgs/workbench_friends.png',
+          name: '个人动态'
+        },{
+          iconUrl: 'https://gitlab.com/toonteam/weex/raw/f9492284d15c3d9ec79b72608768d20cf450cae9/src/views/mine/imgs/person-data.png',
+          name: '个人资料'
+        },{
+          iconUrl: 'https://gitlab.com/toonteam/weex/raw/f9492284d15c3d9ec79b72608768d20cf450cae9/src/views/mine/imgs/workbench_card_setting.png',
+          name: '名片设置'
+        }],
+        groupInfoList: [{
+          iconUrl: 'https://gitlab.com/toonteam/weex/raw/f9492284d15c3d9ec79b72608768d20cf450cae9/src/views/mine/imgs/wallet_trade_data.png',
+          name: '我的公司'
+        },{
+          iconUrl: 'https://gitlab.com/toonteam/weex/raw/f9492284d15c3d9ec79b72608768d20cf450cae9/src/views/mine/imgs/group_icon.png',
+          name: '群组'
+        },{
+          iconUrl: 'https://gitlab.com/toonteam/weex/raw/f9492284d15c3d9ec79b72608768d20cf450cae9/src/views/mine/imgs/person-dynamic.png',
+          name: '审批'
+        }]
+        }
+    },
     methods: {
       jumpA (event) {
         navigator.push(this, route.demoA)
